923123f0eb57927bc492e890620ec36ba44a827b
hill <hill@techfak.uni-bielefeld.de>
2023-09-15 16:51:29 +0200

    Fix transformer dataset length error

--- Transformer_baseline/main.py
+++ Transformer_baseline/main.py
@@ -15,10 +15,14 @@ class TransformerDataset(Dataset):
         return item
 
     def __len__(self):
-        return len(self.encodings)
+        return len(self.labels)
 
 
 if __name__ == "__main__":
+    # Hyperparameters
+    MAX_LENGTH = 200
+    EPOCHS = 3
+
     dataset_train = ValidityNoveltyClassificationDataset("../data/TaskA_train.csv")
     dataset_valid = ValidityNoveltyClassificationDataset("../data/TaskA_dev.csv")
 
@@ -26,15 +30,31 @@ if __name__ == "__main__":
     tokenizer = RobertaTokenizer.from_pretrained("roberta-base")
 
     # Encode the sentences
-    encodings_train = tokenizer(dataset_train.sentences, truncation=True, padding=True)
-    encodings_valid = tokenizer(dataset_valid.sentences, truncation=True, padding=True)
+    encodings_train = tokenizer(
+        dataset_train.sentences,
+        add_special_tokens=True,
+        truncation="longest_first",
+        padding="max_length",
+        max_length=MAX_LENGTH,
+        return_tensors="pt",
+        verbose=False
+    )
+    encodings_valid = tokenizer(
+        dataset_valid.sentences,
+        add_special_tokens=True,
+        truncation="longest_first",
+        padding="max_length",
+        max_length=MAX_LENGTH,
+        return_tensors="pt",
+        verbose=False
+    )
 
     # Train two models, one for each task
-    for task in ["validity", "novelty"]:
+    for task in ["validity"]: # TODO: Add "novelty" later
         # Create the model
         training_args = TrainingArguments(
             output_dir="./results",
-            num_train_epochs=3,
+            num_train_epochs=EPOCHS,
             per_device_train_batch_size=16,
             per_device_eval_batch_size=16,
             warmup_steps=500,
@@ -48,9 +68,6 @@ if __name__ == "__main__":
         # Convert the labels to tensors
         labels_train = torch.tensor(dataset_train.labels[task])
         labels_valid = torch.tensor(dataset_valid.labels[task])
-
-        print(labels_train.shape)
-        print(labels_valid.shape)
 
         # Create the datasets
         transformer_dataset_train = TransformerDataset(encodings_train, labels_train)
