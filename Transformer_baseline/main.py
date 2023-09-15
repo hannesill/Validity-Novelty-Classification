1bd540bfe850d359b73838e333eae6f3b9337565
hill <hill@techfak.uni-bielefeld.de>
2023-09-15 19:37:21 +0200

    Let trainer handle device management instead of dataset class

--- Transformer_baseline/main.py
+++ Transformer_baseline/main.py
@@ -11,8 +11,8 @@ class TransformerDataset(Dataset):
         self.labels = labels
 
     def __getitem__(self, idx):
-        item = {key: val[idx].to(device) for key, val in self.encodings.items()}
-        item['labels'] = self.labels[idx].to(device)
+        item = {key: val[idx] for key, val in self.encodings.items()}
+        item['labels'] = self.labels[idx]
         return item
 
     def __len__(self):
